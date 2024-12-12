6a2dfa9c966b0f0c3ff3502f70b24bc1ae2d83d8
Shishir Pokhrel <pokhrel.sh@northeastern.edu>
2024-12-12 13:52:05 -0500

    add a6

--- Kanbas/Database/grades.js
+++
@@ -1,3 +0,0 @@
-export default [
-    { "_id": "A101", "user": "iron_man", "score": 100 },
-]
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import express from 'express'
-import Hello from "./Hello.js"
+// const express = require('express') // this is old syntax
+import express from "express";
+import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
@@ -7,15 +8,21 @@ import session from "express-session";
 import "dotenv/config";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import mongoose from "mongoose";
+import "dotenv/config";
 
-const app = express()
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
+const app = express();
 app.use(
   cors({
     credentials: true,
     origin: process.env.NETLIFY_URL || "http://localhost:3000",
   })
- );
- const sessionOptions = {
+);
+const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
   saveUninitialized: false,
@@ -29,13 +36,13 @@ if (process.env.NODE_ENV !== "development") {
   };
 }
 app.use(session(sessionOptions));
+
 app.use(express.json());
 
 Lab5(app);
-Hello(app)
+Hello(app);
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
-
-
-app.listen(process.env.PORT || 4000)
+AssignmentRoutes(app);
+app.listen(process.env.PORT || 4000);
